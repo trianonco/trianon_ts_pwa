edea2a61f02c9cd77fdf8a16c8b35c4c3bb40436
Ing. Jorge Luis Mayorga Taboda <40527919+IngJorgeLuisMayorga@users.noreply.github.com>
2019-06-21 14:18:42 -0500

    Deploy

--- src/views/home/components/home-desktop-cover/home-desktop-cover.component.vue
+++ src/views/home/components/home-desktop-cover/home-desktop-cover.component.vue
@@ -1,10 +1,40 @@
 <template>
   <div class="swiper home-desktop-cover">
     <div class="female-category">
-      <div class="button">DAMA</div>
+      <div class="button" @click="UX.isFemaleOpen = !UX.isFemaleOpen">DAMA</div>
+      <div class="categories male" v-bind:class="{ active: UX.isMaleOpen }">
+        <ul>
+          <li v-for="(category,index) of MaleCategories" :key="index">
+            <span>{{category.title}}</span>
+            <img
+              src="../../../../shared/assets/images/gender-categories/bullet-gold.png"
+              v-if="isBulletActive(category)"
+            >
+            <img
+              src="../../../../shared/assets/images/gender-categories/bullet-gray.png"
+              v-if="!isBulletActive(category)"
+            >
+          </li>
+        </ul>
+      </div>
     </div>
     <div class="male-category">
-      <div class="button">HOMBRE</div>
+      <div class="button" @click="UX.isMaleOpen = !UX.isMaleOpen; ">HOMBRE</div>
+      <div class="categories female" v-bind:class="{ active: UX.isFemaleOpen }">
+        <ul>
+          <li v-for="(category,index) of FemaleCategories" :key="index">
+            <span>{{category.title}}</span>
+            <img
+              src="../../../../shared/assets/images/gender-categories/bullet-gold.png"
+              v-if="isBulletActive(category)"
+            >
+            <img
+              src="../../../../shared/assets/images/gender-categories/bullet-gray.png"
+              v-if="!isBulletActive(category)"
+            >
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -23,7 +53,40 @@ import VLazyImage from "v-lazy-image";
   }
 })
 export default class HomeSwiperComponent extends Vue {
+  private UX: any = {
+    isMaleOpen: false,
+    isFemaleOpen: false
+  };
+
+  private MaleCategories: any[] = [
+    { title: "BILLETERAS" },
+    { title: "BOLSOS Y MALETINES" },
+    { title: "CINTURONES" },
+    { title: "LLAVEROS" },
+    { title: "MONEDEROS Y TARJETEROS" },
+    { title: "PORTA BILLETES" },
+    { title: "PORTA DOCUMENTOS" },
+    { title: "PORTA LLAVEROS" },
+    { title: "PORTA PASAPORTES" }
+  ];
+
+  private FemaleCategories: any[] = [
+    { title: "BILLETERAS" },
+    { title: "BOLSOS Y MALETINES" },
+    { title: "CINTURONES" },
+    { title: "LLAVEROS" },
+    { title: "MONEDEROS Y TARJETEROS" },
+    { title: "PORTA BILLETES" },
+    { title: "PORTA COSMÉTICOS" },
+    { title: "PORTA DOCUMENTOS" },
+    { title: "PORTA PASAPORTES" }
+  ];
+
   private async mounted() {}
+
+  private isBulletActive(category: any) {
+    return false;
+  }
 }
 </script>
 
@@ -34,5 +97,120 @@ div.home-desktop-cover {
 }
 @media (min-width: 600px) {
   #constructor-desktop-cover();
+  div.home-desktop-cover {
+    .female-category,
+    .male-category {
+      position: relative;
+    }
+    div.categories {
+      position: absolute;
+      width: 50vw;
+      height: 450px;
+
+      overflow: hidden;
+
+      top: 450px;
+      -webkit-transition: all 500ms ease-in-out;
+      -moz-transition: all 500ms ease-in-out;
+      -ms-transition: all 500ms ease-in-out;
+      -o-transition: all 500ms ease-in-out;
+      transition: all 500ms ease-in-out;
+
+      &.active {
+        top: 0px;
+        -webkit-transition: all 500ms ease-in-out;
+        -moz-transition: all 500ms ease-in-out;
+        -ms-transition: all 500ms ease-in-out;
+        -o-transition: all 500ms ease-in-out;
+        transition: all 500ms ease-in-out;
+      }
+
+      &.male {
+        background-size: cover;
+        background-image: url("./../../../../shared/assets/images/gender-male-categories-bg_Mesa de trabajo 1.png");
+
+        ul {
+          display: block;
+          padding: 0px;
+          margin: 0px;
+          padding: 1em;
+          li {
+            padding: 0px;
+            margin: 0px;
+            padding: 0.5em;
+            text-decoration: underline;
+            text-align: right;
+
+            display: -ms-flexbox;
+            display: -webkit-flex;
+            display: flex;
+            -webkit-flex-direction: row;
+            -ms-flex-direction: row;
+            flex-direction: row;
+            -webkit-flex-wrap: wrap;
+            -ms-flex-wrap: wrap;
+            flex-wrap: wrap;
+            -webkit-justify-content: flex-end;
+            -ms-flex-pack: end;
+            justify-content: flex-end;
+            -webkit-align-content: center;
+            -ms-flex-line-pack: center;
+            align-content: center;
+            -webkit-align-items: center;
+            -ms-flex-align: center;
+            align-items: center;
+            img {
+              width: 10px;
+              padding: 0.5em;
+            }
+          }
+          color: #fff;
+          font-family: "TrajanPro";
+        }
+      }
+      &.female {
+        background-size: cover;
+        background-image: url("./../../../../shared/assets/images/gender-female_Mesa de trabajo 1.png");
+
+        ul {
+          display: block;
+          padding: 0px;
+          margin: 0px;
+          padding: 1em;
+          li {
+            padding: 0px;
+            margin: 0px;
+            padding: 0.5em;
+            text-decoration: underline;
+            text-align: right;
+            display: -ms-flexbox;
+            display: -webkit-flex;
+            display: flex;
+            -webkit-flex-direction: row;
+            -ms-flex-direction: row;
+            flex-direction: row;
+            -webkit-flex-wrap: wrap;
+            -ms-flex-wrap: wrap;
+            flex-wrap: wrap;
+            -webkit-justify-content: flex-start;
+            -ms-flex-pack: start;
+            justify-content: flex-start;
+            -webkit-align-content: center;
+            -ms-flex-line-pack: center;
+            align-content: center;
+            -webkit-align-items: center;
+            -ms-flex-align: center;
+            align-items: center;
+            img {
+              width: 10px;
+              padding: 0.5em;
+            }
+          }
+          color: #fff;
+          font-family: "TrajanPro";
+        }
+      }
+    }
+  }
 }
 </style>
