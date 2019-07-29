a185947d89356861abef68107e5e7281b0e139e8
Ing. Jorge Luis Mayorga Taboda <40527919+IngJorgeLuisMayorga@users.noreply.github.com>
2019-07-29 11:18:53 -0500

    Deploy

--- dist/index.php
+++ dist/index.php
@@ -27,27 +27,45 @@ $IMAGE_SRC = '"https://firebasestorage.googleapis.com/v0/b/trianon-co-pwa-dev.ap
 
 $vue_pwa_html_template = file_get_contents("index-app.html");
 
-$vue_pwa_html_meta_title= '"TRIANON | Fabricación de marroquinería y accesorios en cuero"';
+$vue_pwa_html_meta_title= 'TRIANON | Fabricación de marroquinería y accesorios en cuero';
 $vue_pwa_html_meta_author = ' JORGE LUIS MAYORGA TABORDA ';
 $vue_pwa_html_meta_description = $DESCRIPTION;
 $vue_pwa_html_meta_image = $IMAGE_SRC;
-$vue_pwa_html_meta_keywords= '"billeteras,billeteras colombia,envio a toda colombia,trianon,trianonshoponline,pagosonline,tienda en linea trianon,bolsos,bolsos en cuero,regalo mama ,regalo papa colombia,regalo navidad colombia,pago seguro en linea,cinturon trianon,dia del padre,billete"';
+$vue_pwa_html_meta_keywords= 'billeteras,billeteras colombia,envio a toda colombia,trianon,trianonshoponline,pagosonline,tienda en linea trianon,bolsos,bolsos en cuero,regalo mama ,regalo papa colombia,regalo navidad colombia,pago seguro en linea,cinturon trianon,dia del padre,billete';
 
 $vue_pwa_html_meta_schema_name = $TITLE;
 $vue_pwa_html_meta_schema_description = $DESCRIPTION;
 $vue_pwa_html_meta_schema_image = $IMAGE_SRC;
 
 $vue_pwa_html_meta_twitter_title = $TITLE;
-$vue_pwa_html_meta_twitter_card = '"summary"';
+$vue_pwa_html_meta_twitter_card = 'summary';
 $vue_pwa_html_meta_twitter_description =  $DESCRIPTION;
 
 $vue_pwa_html_meta_og_title = $TITLE;
 $vue_pwa_html_meta_og_description = $DESCRIPTION;
 $vue_pwa_html_meta_og_image = $IMAGE_SRC;
-$vue_pwa_html_meta_og_url = '"https://www.trianon.com.co/"';
+$vue_pwa_html_meta_og_url = 'https://www.trianon.com.co/';
 $vue_pwa_html_meta_og_site_name = '"TRIANON"';
 
 $vue_pwa_html_template = str_replace('<title>TRIANON | Productos 100% colombianos</title>', $vue_pwa_html_meta_title, $vue_pwa_html_template);
+$vue_pwa_html_template = str_replace('META_AUTHOR', $vue_pwa_html_meta_author, $vue_pwa_html_template);
+$vue_pwa_html_template = str_replace('META_KEYWORDS', $vue_pwa_html_meta_keywords, $vue_pwa_html_template);
+$vue_pwa_html_template = str_replace('META_DESCRIPTION', $vue_pwa_html_meta_description, $vue_pwa_html_template);
+$vue_pwa_html_template = str_replace('META_IMAGE', $vue_pwa_html_meta_image, $vue_pwa_html_template);
+ 
+$vue_pwa_html_template = str_replace('META_SCHEMA_NAME',  $vue_pwa_html_meta_schema_name, $vue_pwa_html_template);
+$vue_pwa_html_template = str_replace('META_SCHEMA_DESCRIPTION',  $vue_pwa_html_meta_schema_description, $vue_pwa_html_template);
+$vue_pwa_html_template = str_replace('META_SCHEMA_IMAGE',  $vue_pwa_html_meta_schema_image, $vue_pwa_html_template);
+ 
+$vue_pwa_html_template = str_replace('META_TWITTER_TITLE',  $vue_pwa_html_meta_twitter_title, $vue_pwa_html_template);
+$vue_pwa_html_template= str_replace('META_TWITTER_CARD',  $vue_pwa_html_meta_twitter_card, $vue_pwa_html_template);
+$vue_pwa_html_template= str_replace('META_TWITTER_DESCRIPTION',  $vue_pwa_html_meta_twitter_description, $vue_pwa_html_template);
+ 
+$vue_pwa_html_template = str_replace('META_OG_TITLE',  $vue_pwa_html_meta_og_title, $vue_pwa_html_template);
+$vue_pwa_html_template = str_replace('META_OG_DESCRIPTION',  $vue_pwa_html_meta_og_description, $vue_pwa_html_template);
+$vue_pwa_html_template = str_replace('META_OG_IMAGE',  $vue_pwa_html_meta_og_image, $vue_pwa_html_template);
+$vue_pwa_html_template = str_replace('META_OG_URL',  $vue_pwa_html_meta_og_url, $vue_pwa_html_template);
+$vue_pwa_html_template = str_replace('META_OG_SITE_NAME',  $vue_pwa_html_meta_og_site_name, $vue_pwa_html_template);
 
 echo $vue_pwa_html_template ;
 
--- dist/precache-manifest.5d8133b9a98c6c9239b4042caaa4a41b.js
+++ dist/precache-manifest.5d8133b9a98c6c9239b4042caaa4a41b.js
@@ -8,12 +8,12 @@ self.__precacheManifest = [
     "url": "/maintain.html"
   },
   {
-    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/robots.txt"
-  },
-  {
     "revision": "dadb3ed10943a11cf84bcb2717d29e0c",
     "url": "/redirect.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
   },
   {
     "revision": "be95e66a21b26c932482f3db7b84b410",
@@ -208,12 +208,12 @@ self.__precacheManifest = [
     "url": "/fonts/OpenSansLight-Italic.dc5dda5c.woff"
   },
   {
-    "revision": "6ad659c0c8246df36fc8bab6fa2f0966",
-    "url": "/fonts/OpenSans-Italic.6ad659c0.woff"
-  },
-  {
     "revision": "db92f5a3582fbe6289eb1f5beaaaa0f2",
     "url": "/fonts/OpenSans-ExtraboldItalic.db92f5a3.woff"
+  },
+  {
+    "revision": "6ad659c0c8246df36fc8bab6fa2f0966",
+    "url": "/fonts/OpenSans-Italic.6ad659c0.woff"
   },
   {
     "revision": "5982be5666dbac0413d9ac38b011d87b",
@@ -224,12 +224,12 @@ self.__precacheManifest = [
     "url": "/fonts/OpenSans-Bold.05d1a99a.woff"
   },
   {
-    "revision": "90327f5ac43d0fbc708f3653ffc0a043",
-    "url": "/fonts/OpenSans.90327f5a.woff"
-  },
-  {
     "revision": "cee4f07ed7988e917b69db983549fe65",
     "url": "/fonts/OpenSans-Extrabold.cee4f07e.woff"
+  },
+  {
+    "revision": "90327f5ac43d0fbc708f3653ffc0a043",
+    "url": "/fonts/OpenSans.90327f5a.woff"
   },
   {
     "revision": "4c7b8237f45b448efb609f5d8e3997a9",
@@ -264,20 +264,24 @@ self.__precacheManifest = [
     "url": "/fonts/OpenSans-BoldItalic.12458244.eot"
   },
   {
-    "revision": "10ba07da64cc6a490da4ac3af4dd238b",
-    "url": "/fonts/OpenSans.10ba07da.ttf"
+    "revision": "2c06d8510807447c42162c50f5f736d6",
+    "url": "/fonts/OpenSansLight-Italic.2c06d851.ttf"
   },
   {
     "revision": "05cb165500848d34ff71",
     "url": "/js/AuthorizedDistributors.2890ec9c.js"
   },
   {
-    "revision": "2c06d8510807447c42162c50f5f736d6",
-    "url": "/fonts/OpenSansLight-Italic.2c06d851.ttf"
+    "revision": "10ba07da64cc6a490da4ac3af4dd238b",
+    "url": "/fonts/OpenSans.10ba07da.ttf"
   },
   {
     "revision": "66366f515077a2e906ef",
     "url": "/js/about.b6b1bc09.js"
+  },
+  {
+    "revision": "58e9b403dbc8d7ef0b39e11ada61889c",
+    "url": "/fonts/OpenSans-ExtraboldItalic.58e9b403.eot"
   },
   {
     "revision": "e6c21381ad4f8eaff7c5bbbb2623befa",
@@ -288,56 +292,52 @@ self.__precacheManifest = [
     "url": "/fonts/OpenSans_1.10ba07da.ttf"
   },
   {
-    "revision": "58e9b403dbc8d7ef0b39e11ada61889c",
-    "url": "/fonts/OpenSans-ExtraboldItalic.58e9b403.eot"
-  },
-  {
     "revision": "55bfb3e43457ec893f128c2d73f76457",
     "url": "/fonts/OpenSans-Bold.55bfb3e4.eot"
-  },
-  {
-    "revision": "885e3d6f49fb6783494654e69793e802",
-    "url": "/fonts/OpenSans.885e3d6f.eot"
   },
   {
     "revision": "0fc8bb5af95997edafd9e975b3eb3ed7",
     "url": "/fonts/OpenSans-Bold.0fc8bb5a.ttf"
   },
   {
-    "revision": "885e3d6f49fb6783494654e69793e802",
-    "url": "/fonts/OpenSans_1.885e3d6f.eot"
-  },
-  {
     "revision": "fa25da4ad6b2e2e9484c771d7943deb1",
     "url": "/fonts/OpenSans-Extrabold.fa25da4a.eot"
+  },
+  {
+    "revision": "885e3d6f49fb6783494654e69793e802",
+    "url": "/fonts/OpenSans.885e3d6f.eot"
   },
   {
     "revision": "8165c1ec52bb9262b2105edc8032a5db",
     "url": "/fonts/OpenSans-Semibold.8165c1ec.eot"
   },
   {
+    "revision": "885e3d6f49fb6783494654e69793e802",
+    "url": "/fonts/OpenSans_1.885e3d6f.eot"
+  },
+  {
     "revision": "22d4a36b7d366b2b213801c716dfb0b7",
     "url": "/fonts/OpenSans-Semibold.22d4a36b.ttf"
-  },
-  {
-    "revision": "e1d396939867c493f06f2bbc071993d9",
-    "url": "/fonts/OpenSans-Light.e1d39693.eot"
-  },
-  {
-    "revision": "5299020bf84d03883fa452ee7478c60f",
-    "url": "/fonts/OpenSans-Light.5299020b.ttf"
-  },
-  {
-    "revision": "27b48950afd77b0e4aaa98981c1b8813",
-    "url": "/fonts/TrajanPro-Bold.27b48950.eot"
   },
   {
     "revision": "14633e007efb0b390d06cd0a30789962",
     "url": "/fonts/TrajanPro-Bold.14633e00.ttf"
   },
   {
+    "revision": "27b48950afd77b0e4aaa98981c1b8813",
+    "url": "/fonts/TrajanPro-Bold.27b48950.eot"
+  },
+  {
+    "revision": "5299020bf84d03883fa452ee7478c60f",
+    "url": "/fonts/OpenSans-Light.5299020b.ttf"
+  },
+  {
     "revision": "e8425e8a393b5d1e7fe417f43e520c54",
     "url": "/fonts/TrajanPro-Regular.e8425e8a.ttf"
+  },
+  {
+    "revision": "e1d396939867c493f06f2bbc071993d9",
+    "url": "/fonts/OpenSans-Light.e1d39693.eot"
   },
   {
     "revision": "4036090917114a9f089af14dc8b9dffc",
@@ -516,16 +516,16 @@ self.__precacheManifest = [
     "url": "/index.html"
   },
   {
-    "revision": "cf170376925acce9b6bf",
-    "url": "/js/AuthorizedShops.fcae10e7.js"
+    "revision": "afab21baff3ded9e1f2d283b75099ebc",
+    "url": "/index.php"
   },
   {
     "revision": "bb4253da032fbfccb0b0a7ef687ea4b2",
     "url": "/index-app.html"
   },
   {
-    "revision": "d5d4093e44ea9e02ae16e6e4b4d1a9d2",
-    "url": "/index.php"
+    "revision": "cf170376925acce9b6bf",
+    "url": "/js/AuthorizedShops.fcae10e7.js"
   },
   {
     "revision": "cf170376925acce9b6bf",
