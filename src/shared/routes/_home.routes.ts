31c29bdee6ecb70c4b88859014ee5c527e1b0049
Ing. Jorge Luis Mayorga Taboda <40527919+IngJorgeLuisMayorga@users.noreply.github.com>
2019-08-10 12:31:19 -0500

    CleanDeploy

--- src/shared/routes/_home.routes.ts
+++ src/shared/routes/_home.routes.ts
@@ -1,10 +1,14 @@
-import HomeView from "@/views/home/home.view.vue";
-
+import HomeView from '@/views/home/home.view.vue';
 
 export const HOME_ROUTES = [
-    {
-        path: "/",
-        name: "home",
-        component: HomeView
-    }
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/home',
+    name: 'home2',
+    component: HomeView
+  }
 ];
