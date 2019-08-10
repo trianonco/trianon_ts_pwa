effebe6d1b1500cacfb950ef3f2111f4aeea9e62
Ing. Jorge Luis Mayorga Taboda <40527919+IngJorgeLuisMayorga@users.noreply.github.com>
2019-08-10 11:41:29 -0500

    CleanDeploy

--- public/index.php
+++ public/index.php
@@ -1,7 +1,9 @@
 <?php
 
-header("Cache-Control: no-cache, must-revalidate");
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 header("Content-Type: application/xml; charset=utf-8");
 
 $REQUEST_URI = $_SERVER['REQUEST_URI'];
