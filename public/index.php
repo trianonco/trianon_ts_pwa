756e38ba60c8cfe77bff97212859aa345c27369e
Ing. Jorge Luis Mayorga Taboda <40527919+IngJorgeLuisMayorga@users.noreply.github.com>
2019-08-10 11:38:27 -0500

    CleanDeploy

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
-header('Content-type: text/html; charset=UTF-8');
+
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Content-Type: application/xml; charset=utf-8");
 
 $REQUEST_URI = $_SERVER['REQUEST_URI'];
 $REQUEST_URI_PARAMETERS = explode('/', $REQUEST_URI);
