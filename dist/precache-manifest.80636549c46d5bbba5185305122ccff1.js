1b96a11e6d3f2634b62236be357c9965ddc5f434
Ing. Jorge Luis Mayorga Taboda <40527919+IngJorgeLuisMayorga@users.noreply.github.com>
2019-08-10 10:56:42 -0500

    Deploy

--- dist/precache-manifest.80636549c46d5bbba5185305122ccff1.js
+++ dist/precache-manifest.80636549c46d5bbba5185305122ccff1.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = [
   {
-    "revision": "ecbfae5924935c60785e9e2a36f8e05f",
-    "url": "/fonts/OpenSansLight-Italic.ecbfae59.eot"
-  },
-  {
-    "revision": "bfe0522711522156ef72c996805bfcb4",
-    "url": "/maintain.html"
+    "revision": "2c06d8510807447c42162c50f5f736d6",
+    "url": "/fonts/OpenSansLight-Italic.2c06d851.ttf"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
     "url": "/robots.txt"
   },
   {
-    "revision": "dadb3ed10943a11cf84bcb2717d29e0c",
-    "url": "/redirect.html"
+    "revision": "10ba07da64cc6a490da4ac3af4dd238b",
+    "url": "/fonts/OpenSans.10ba07da.ttf"
   },
   {
-    "revision": "e6c21381ad4f8eaff7c5bbbb2623befa",
-    "url": "/fonts/OpenSans-Italic.e6c21381.ttf"
+    "revision": "cf170376925acce9b6bf",
+    "url": "/css/AuthorizedShops.4aa0a016.css"
   },
   {
     "revision": "66366f515077a2e906ef",
     "url": "/css/about.58132e3d.css"
+  },
+  {
+    "revision": "66366f515077a2e906ef",
+    "url": "/js/about.b6b1bc09.js"
   },
   {
     "revision": "f79756f2c7e60e0fd51b",
@@ -236,10 +236,6 @@ self.__precacheManifest = [
     "url": "/fonts/OpenSans-Extrabold.cee4f07e.woff"
   },
   {
-    "revision": "90327f5ac43d0fbc708f3653ffc0a043",
-    "url": "/fonts/OpenSans_1.90327f5a.woff"
-  },
-  {
     "revision": "12d2fa3dcf227b9538084a6bcfeafcf0",
     "url": "/fonts/TrajanPro-Bold.12d2fa3d.otf"
   },
@@ -248,40 +244,44 @@ self.__precacheManifest = [
     "url": "/fonts/OpenSans-Light.87051a9d.woff"
   },
   {
-    "revision": "b11f35f77210c651367969365f87af24",
-    "url": "/fonts/TrajanPro-Regular.b11f35f7.otf"
+    "revision": "90327f5ac43d0fbc708f3653ffc0a043",
+    "url": "/fonts/OpenSans_1.90327f5a.woff"
   },
   {
-    "revision": "427dc6119e442413329f5901bab5161d",
-    "url": "/fonts/OpenSans-SemiboldItalic.427dc611.eot"
+    "revision": "b11f35f77210c651367969365f87af24",
+    "url": "/fonts/TrajanPro-Regular.b11f35f7.otf"
   },
   {
     "revision": "f42a8498d930df4e0d764a8c67b18fed",
     "url": "/fonts/OpenSans-SemiboldItalic.f42a8498.ttf"
   },
   {
+    "revision": "427dc6119e442413329f5901bab5161d",
+    "url": "/fonts/OpenSans-SemiboldItalic.427dc611.eot"
+  },
+  {
     "revision": "1245824481137ab66a6f540116a569ab",
     "url": "/fonts/OpenSans-BoldItalic.12458244.eot"
   },
   {
-    "revision": "2c06d8510807447c42162c50f5f736d6",
-    "url": "/fonts/OpenSansLight-Italic.2c06d851.ttf"
+    "revision": "ecbfae5924935c60785e9e2a36f8e05f",
+    "url": "/fonts/OpenSansLight-Italic.ecbfae59.eot"
   },
   {
     "revision": "05cb165500848d34ff71",
     "url": "/js/AuthorizedDistributors.2890ec9c.js"
   },
   {
-    "revision": "10ba07da64cc6a490da4ac3af4dd238b",
-    "url": "/fonts/OpenSans.10ba07da.ttf"
-  },
-  {
-    "revision": "66366f515077a2e906ef",
-    "url": "/js/about.b6b1bc09.js"
+    "revision": "cf170376925acce9b6bf",
+    "url": "/js/AuthorizedShops.fcae10e7.js"
   },
   {
     "revision": "be95e66a21b26c932482f3db7b84b410",
     "url": "/fonts/OpenSans-Italic.be95e66a.eot"
+  },
+  {
+    "revision": "e6c21381ad4f8eaff7c5bbbb2623befa",
+    "url": "/fonts/OpenSans-Italic.e6c21381.ttf"
   },
   {
     "revision": "58e9b403dbc8d7ef0b39e11ada61889c",
@@ -296,44 +296,44 @@ self.__precacheManifest = [
     "url": "/fonts/OpenSans-Bold.55bfb3e4.eot"
   },
   {
-    "revision": "0fc8bb5af95997edafd9e975b3eb3ed7",
-    "url": "/fonts/OpenSans-Bold.0fc8bb5a.ttf"
-  },
-  {
     "revision": "885e3d6f49fb6783494654e69793e802",
     "url": "/fonts/OpenSans.885e3d6f.eot"
+  },
+  {
+    "revision": "0fc8bb5af95997edafd9e975b3eb3ed7",
+    "url": "/fonts/OpenSans-Bold.0fc8bb5a.ttf"
   },
   {
     "revision": "8165c1ec52bb9262b2105edc8032a5db",
     "url": "/fonts/OpenSans-Semibold.8165c1ec.eot"
   },
   {
-    "revision": "fa25da4ad6b2e2e9484c771d7943deb1",
-    "url": "/fonts/OpenSans-Extrabold.fa25da4a.eot"
+    "revision": "885e3d6f49fb6783494654e69793e802",
+    "url": "/fonts/OpenSans_1.885e3d6f.eot"
   },
   {
     "revision": "22d4a36b7d366b2b213801c716dfb0b7",
     "url": "/fonts/OpenSans-Semibold.22d4a36b.ttf"
   },
   {
-    "revision": "885e3d6f49fb6783494654e69793e802",
-    "url": "/fonts/OpenSans_1.885e3d6f.eot"
+    "revision": "fa25da4ad6b2e2e9484c771d7943deb1",
+    "url": "/fonts/OpenSans-Extrabold.fa25da4a.eot"
   },
   {
-    "revision": "e1d396939867c493f06f2bbc071993d9",
-    "url": "/fonts/OpenSans-Light.e1d39693.eot"
+    "revision": "14633e007efb0b390d06cd0a30789962",
+    "url": "/fonts/TrajanPro-Bold.14633e00.ttf"
   },
   {
     "revision": "5299020bf84d03883fa452ee7478c60f",
     "url": "/fonts/OpenSans-Light.5299020b.ttf"
   },
   {
-    "revision": "27b48950afd77b0e4aaa98981c1b8813",
-    "url": "/fonts/TrajanPro-Bold.27b48950.eot"
+    "revision": "e1d396939867c493f06f2bbc071993d9",
+    "url": "/fonts/OpenSans-Light.e1d39693.eot"
   },
   {
-    "revision": "14633e007efb0b390d06cd0a30789962",
-    "url": "/fonts/TrajanPro-Bold.14633e00.ttf"
+    "revision": "27b48950afd77b0e4aaa98981c1b8813",
+    "url": "/fonts/TrajanPro-Bold.27b48950.eot"
   },
   {
     "revision": "4036090917114a9f089af14dc8b9dffc",
@@ -516,16 +516,16 @@ self.__precacheManifest = [
     "url": "/index.html"
   },
   {
-    "revision": "cf170376925acce9b6bf",
-    "url": "/js/AuthorizedShops.fcae10e7.js"
+    "revision": "bfe0522711522156ef72c996805bfcb4",
+    "url": "/maintain.html"
   },
   {
-    "revision": "cc67800677d0e38246f481f4f653a2c6",
+    "revision": "f1499135905c3690e5524ee9134765ca",
     "url": "/index.php"
   },
   {
-    "revision": "cf170376925acce9b6bf",
-    "url": "/css/AuthorizedShops.4aa0a016.css"
+    "revision": "dadb3ed10943a11cf84bcb2717d29e0c",
+    "url": "/redirect.html"
   },
   {
     "revision": "05cb165500848d34ff71",
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.912920058e903d76521ff79d3f6ad93c.js"
+  "/precache-manifest.80636549c46d5bbba5185305122ccff1.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "trianon_ts_pwa"});
