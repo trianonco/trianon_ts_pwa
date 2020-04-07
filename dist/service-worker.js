126bd1d045de2293405de00775449bb14ac410fc
Ing. Jorge Luis Mayorga Taboda <40527919+IngJorgeLuisMayorga@users.noreply.github.com>
2020-04-07 17:21:51 -0500

    Covid message

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b4aebaea22452ebb2e8ebebe2d80ecc5.js"
+  "/precache-manifest.b5911b34ccfafb6be005d19384162fb3.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "trianon_ts_pwa"});
